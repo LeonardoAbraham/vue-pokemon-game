2fbdfec8012a714d5d1c369a8b5e908606200ccd
Leonardo <leon.alonzo.leo@gmail.com>
2022-01-19 23:14:43 -0600

    About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
-    <h1>Pagina de la API pokémon</h1>
-    <a href="https://pokeapi.co/">API</a>
+    <h1>Página de la API pokémon</h1>
+    <a href="https://pokeapi.co/">https://pokeapi.co/</a>
+
+    <h1>Leonardo Abraham Alonzo Martinez</h1>
+    <a href="https://github.com/LeonardoAbraham/vue-pokemon-game">https://github.com/LeonardoAbraham/vue-pokemon-game</a>
+
+    <h2>Curso UDEMY: Vue.js - Fernando Herrera</h2>
+    <a href="https://www.udemy.com/course/vuejs-fh/">https://www.udemy.com/course/vuejs-fh/</a>    
   </div>
 </template>
