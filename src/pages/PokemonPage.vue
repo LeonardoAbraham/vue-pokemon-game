7002a37186d0b4047d9f00eb6cc332fb6d55b4df
Leonardo <leon.alonzo.leo@gmail.com>
2022-01-19 22:16:18 -0600

    Seleccionar un pokémon aleatoriamente

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,8 +1,10 @@
 <template>
-    <h1>¿Quién es este pokémon?</h1>
-    <PokemonPicture :pokemonId="25" :showPokemon="true"></PokemonPicture>
-    <PokemonOptions :pokemons="pokemonArr" />
-
+    <h1 v-if="!pokemon">Espere por favor...</h1>
+    <div v-else>
+        <h1>¿Quién es este pokémon?</h1>
+        <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon"></PokemonPicture>
+        <PokemonOptions :pokemons="pokemonArr" />
+    </div>
 </template>
 <script>
 
@@ -18,11 +20,17 @@ export default {
     data() {
         return {
             pokemonArr: [],
+            pokemon: null,
+            showPokemon: false,
         }
     },
     methods: {
         async mixPokemonArray(){
             this.pokemonArr = await getPokemonOptions()
+
+            const rndInt = Math.floor( Math.random() * 4 )
+
+            this.pokemon = this.pokemonArr[rndInt]
         }
     },
     mounted() {
