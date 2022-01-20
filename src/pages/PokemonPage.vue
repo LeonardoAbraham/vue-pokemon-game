63b014ea68247f0b40c32c6b700d6372af8e1118
Leonardo <leon.alonzo.leo@gmail.com>
2022-01-19 18:52:50 -0600

    creación de estructura de componentes

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>¿Quién es este pokémon?</h1>
+    <!-- Componente Imagen pokémon-->
+    <PokemonPicture></PokemonPicture>
+    <PokemonOptions/>
+
+    <!-- Componente Opciones -->
+</template>
+<script>
+
+import PokemonOptions from '@/components/PokemonOptions'
+import PokemonPicture from '@/components/PokemonPicture'
+export default {
+    components:{
+        PokemonPicture, 
+        PokemonOptions
+    },
+
+}
+</script>
+<style lang="">
+    
+</style>
